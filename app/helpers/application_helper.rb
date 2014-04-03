659cf71b759d48e43e4fc9efedbc7802cbdb3090
jay16 <jay_li@intfocus.com>
2014-04-04 00:11:12 +0800

    modify contact page'css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def controller_stylesheet_link_tag
+    css_file = case controller_name
+    when "home"
+      "https://land.xiaomi.net/res/c9369bd/css/micloudfe/home.css"
+    end
+    return "" if css_file.blank?
+    raw %(<link href="#{css_file}" rel="stylesheet" data-turbolinks-track />)
+  end
 end
