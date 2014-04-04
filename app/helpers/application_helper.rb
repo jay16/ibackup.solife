66c88c0747617e7beebd55db416d81faa3acd8b9
jay16 <jay_li@intfocus.com>
2014-04-04 10:30:33 +0800

    header#icon use qiniu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
+#encoding: utf-8
 module ApplicationHelper
+
+  def render_page_title
+    content_tag(:title, "爱备份", nil, false)
+  end
 
   def controller_stylesheet_link_tag
     case controller_name
@@ -9,4 +14,17 @@ module ApplicationHelper
 
   def controller_javascript_include_tag
   end
+
+  MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+                        'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+                        'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+                        'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+                        'webos|amoi|novarra|cdm|alcatel|pocket|iphone|mobileexplorer|mobile'
+  def mobile?
+    agent_str = request.user_agent.to_s.downcase
+    return false if agent_str =~ /ipad/
+    agent_str =~ Regexp.new(MOBILE_USER_AGENTS)
+  end
+
+  
 end
