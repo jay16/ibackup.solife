a29d06942afa77849ae0244bca97878262dd136a
jay16 <jay_li@intfocus.com>
2014-04-04 10:09:02 +0800

    change bootstrap link with CDN

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 module ApplicationHelper
+
   def controller_stylesheet_link_tag
-    css_file = case controller_name
-    when "home"
-      "https://land.xiaomi.net/res/c9369bd/css/micloudfe/home.css"
+    case controller_name
+    when "home","contacts","sms"
+      stylesheet_link_tag "https://land.xiaomi.net/res/c9369bd/css/micloudfe/home.css"
     end
-    return "" if css_file.blank?
-    raw %(<link href="#{css_file}" rel="stylesheet" data-turbolinks-track />)
+  end
+
+  def controller_javascript_include_tag
   end
 end
